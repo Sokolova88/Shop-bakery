0fa526af100535e600b5f868f0f27fa01c705736
Sokolova88 <[sokolova.studies@gmail.com]>
2024-01-15 15:16:24 +0200

    Add npm swiper and its settings

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,4 @@
 import mobileNav from './modules/mobile-nav.js';
 mobileNav();
+
+import swiper from './modules/swiper.js';
---
+++ src/js/modules/swiper.js
@@ -0,0 +1,36 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation, Pagination],
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  grabCursor: true,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: '.button__slider.button-next',
+    prevEl: '.button__slider.button-prev',
+  },
+});
+
+export default swiper;
